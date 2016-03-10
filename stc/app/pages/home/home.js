f89373abd4a18296fef5e2582d04fd9fc4b3a71b
Greg Marine <gregmarine@iccnet.org>
2016-03-10 02:25:39 +0000

    Setup sidemenu Ionic 2 project - Home & Account pages created

---
+++ stc/app/pages/home/home.js
@@ -0,0 +1,20 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the HomePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/home/home.html',
+})
+export class HomePage {
+  static get parameters() {
+    return [[NavController]];
+  }
+
+  constructor(nav) {
+    this.nav = nav;
+  }
+}
